d0dc94f4ebedb346c0adfdbec66a37813f8c6306
hs-yonekura <y-yonekura@science.co.jp>
2020-03-08 16:07:19 +0900

    fixed

--- assets/view-bigimg-plugin.js
+++ assets/view-bigimg-plugin.js
@@ -1,11 +1,9 @@
 require(["gitbook"], function(gitbook) {
   gitbook.events.bind("page.change", function() {
     var viewer = new ViewBigimg()
-    console.log("s")
 
     var wrap = document.querySelectorAll('.wrap')
     wrap.forEach(function(w){
-      console.log(w)
       w.onclick = function (e) {
         if (e.target.nodeName === 'IMG') {
           viewer.show(e.target.src)
